51cb4c479770452a8223add879de91a61e54b0ce
Feridun Tuncer <82268345+Feroov@users.noreply.github.com>
2024-04-24 12:50:56 +0200

    Update exercise.js

--- pages/api/exercise.js
+++ pages/api/exercise.js
@@ -2,28 +2,21 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  // The path to your json file in the public directory
   const jsonFilePath = path.join(process.cwd(), 'public', 'fitvision.json');
 
   fs.readFile(jsonFilePath, 'utf8', (err, data) => {
     if (err) {
-      // If there's an error reading the file, return a 500 error
       res.status(500).json({ message: 'Error reading the json file' });
     } else {
-      // Parse the JSON data
-      const exercises = JSON.parse(data);
+      let exercises = JSON.parse(data).exercises;
+      exercises = exercises.map(exercise => ({
+        ...exercise,
+        imageUrl: `/media/${exercise.name.toLowerCase().replace(/\s+/g, '-')}.jpg`,
+        videoUrl: `/media/${exercise.name.toLowerCase().replace(/\s+/g, '-')}.mp4`  // Assuming videos are stored similarly to images
+      }));
 
-      // Update the image URLs to point to the local media directory
-      exercises.forEach(exercise => {
-        exercise.imageUrl = `/media/${exercise.name.toLowerCase().replace(/\s+/g, '-')}.jpg`;
-      });
-
-      // Stringify the modified JSON data
-      const updatedData = JSON.stringify(exercises);
-
-      // Set the appropriate headers and send the updated JSON data
       res.setHeader('Content-Type', 'application/json');
-      res.status(200).end(updatedData);
+      res.status(200).json({ exercises });
     }
   });
 }
